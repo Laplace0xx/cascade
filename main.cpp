168835bf93dfa43f4b4fc7524f01fdfa9f69c1ca
Simon Nungwa <simonnungwa99@gmail.com>
2026-04-29 23:03:30 +0100

    Raylib screen

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
-class Enemy {
-    // Enemy class implementation
-};
+#include "raylib.h"
+
+int main(void) {
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Cascade");
+}
